9aa49b228b5b0f316fc5d4307d4eeb3971d5e9d1
raghava <raghava@DESKTOP-IIA9DC9.mshome.net>
2019-07-19 12:19:01 +0530

    Tsting commit

--- SB_Git_R/src/main/java/com/example/demo/SbGitRApplication.java
+++ SB_Git_R/src/main/java/com/example/demo/SbGitRApplication.java
@@ -5,9 +5,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SbGitRApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SbGitRApplication.class, args);
-	}
-
+	
 }
